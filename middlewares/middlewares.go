ddeddc92b3890609dff0972f444be5e64baa6118
nguyenkhoipham1410 <nguyenkhoipham1410@gmail.com>
2023-03-09 16:33:54 +0700

    add cors for client

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -11,6 +11,7 @@ func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 		next(w, r)
 	}
 }
